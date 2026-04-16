1fcee82bd7091c153f9e68bcebc8c035b31df625
Robert Eric Reeves <robertericreeves@gmail.com>
2026-04-16 13:49:21 -0500

    Update dependencies to 1.8.7

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib"))
-	implementation("com.datadatdat:remote-sdk:1.8.6")
+	implementation("com.datadatdat:remote-sdk:1.8.7")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 	testImplementation("io.mockk:mockk:1.14.9")
 }
