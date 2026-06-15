5ab5fc7d4843e7711dc13fa52e6f10b67b86afe1
Robert Eric Reeves <robertericreeves@gmail.com>
2026-06-15 16:13:24 -0500

    Update dependencies to 1.9.8

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib"))
-	implementation("dev.dit:remote-sdk:1.9.7")
+	implementation("dev.dit:remote-sdk:1.9.8")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 	testImplementation("io.mockk:mockk:1.14.11")
 }
